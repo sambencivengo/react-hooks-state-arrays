9d1de493dc5813b1c4dcc91cecfa011af6e8aa3f
Sam Bencivengo <sambencivengo@gmail.com>
2021-10-17 10:00:45 -0400

    commit tree f189fe057dc297554a6df908121c3c07253e9c49

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -1,20 +1,46 @@
-import React, { useState } from "react";
-import { spicyFoods, getNewSpicyFood } from "../data";
+import React, { useState } from 'react';
+import { spicyFoods, getNewSpicyFood } from '../data';
 
 function SpicyFoodList() {
-  const [foods, setFoods] = useState(spicyFoods);
+	const [foods, setFoods] = useState(spicyFoods);
 
-  function handleAddFood() {
-    const newFood = getNewSpicyFood();
-    console.log(newFood);
-  }
+	const foodList = foods.map((food) => (
+		<li onClick={() => handleLiClick(food.id)} key={food.id}>
+			{food.name} | Heat level: {food.heatLevel} | {food.cuisine}
+		</li>
+	));
 
-  return (
-    <div>
-      <button onClick={handleAddFood}>Add New Food</button>
-      <ul>{/* list of spicy foods */}</ul>
-    </div>
-  );
+	function handleAddFood() {
+		const newFood = getNewSpicyFood();
+		const newFoodList = [...foods, newFood];
+		setFoods(newFoodList);
+		console.log(newFood);
+	}
+	function handleLiClick(id) {
+		const newFoodArray = foods.map((food) => {
+			if (food.id === id) {
+				return {
+					...food,
+					heatLevel: food.heatLevel + 1,
+				};
+			} else {
+				return food;
+			}
+		});
+
+		setFoods(newFoodArray);
+
+		// FILTER METHOD
+		// const newFoodArray = foods.filter((food) => food.id !== id);
+		// console.log('This is the newFoodArray: ', newFoodArray);
+		// setFoods(newFoodArray);
+	}
+	return (
+		<div>
+			<button onClick={handleAddFood}>Add New Food</button>
+			<ul>{foodList}</ul>
+		</div>
+	);
 }
 
 export default SpicyFoodList;
